6d67e2a974c729ad3b4c0b6d7eb5997f1a26ffca
Gabriel Jesus <gabriel.developer2@gmail.com>
2024-02-14 23:28:42 -0300

    feat: access library device user

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import { Text, VStack } from "@gluestack-ui/themed";
-import { SafeAreaView } from 'react-native';
+import { ScrollView, Text, VStack, View } from "@gluestack-ui/themed";
+import { Image, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
 import { useTheme } from "styled-components/native";
 import LogoSVG from '../../assets/Logo.svg';
 import { Button } from "../../components/Button";
@@ -9,50 +9,102 @@ import { useHome } from "./useHome";
 
 export function Home() {
   const { colors, fontFamily } = useTheme()
-  const { handleCamera } = useHome()
+  const { handleCamera, images, handleRemovePhoto } = useHome()
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
 
-      <VStack
-        flex={1}
-        backgroundColor={colors.primary}
-      >
         <VStack
-          alignItems="center"
+          flex={1}
+          backgroundColor={colors.primary}
         >
-          <LogoSVG
-            width={200}
-            height={200}
-          />
-          <Text
-            color={colors.secondary}
-            fontSize={16}
-            fontFamily={fontFamily.montserratBold}
+          <VStack
+            alignItems="center"
           >
-            Cadastre seu bolo
-          </Text>
-        </VStack>
-        <VStack
-          marginHorizontal={15}
-        >
-          <Input
-            label="Nome"
-          />
-          <Input
-            label="Descrição"
-          />
-          <ButtonImage  
-            onPress={handleCamera}
-          />
-          <Button
-            title="Enviar"
-            onPress={() => {}}
-            loading={false}
-            disabled={true}
-          />
+            <LogoSVG
+              width={200}
+              height={200}
+            />
+            <Text
+              color={colors.secondary}
+              fontSize={16}
+              fontFamily={fontFamily.montserratBold}
+            >
+              Cadastre seu bolo
+            </Text>
+          </VStack>
+          <VStack
+            marginHorizontal={15}
+            paddingBottom={140}
+            flex={1}
+          >
+            <Input
+              label="Nome"
+            />
+            <Input
+              label="Descrição"
+            />
+            {images.length > 0 ?  (
+              <View
+                flex={1}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <View
+                  width={200}
+                  height={200}
+                >
+                  <Image
+                    source={{ uri: images[0]?.uri }}
+                    alt={images[0]?.filename}
+                    width={200}
+                    height={200}
+                  />
+                  <TouchableOpacity 
+                    style={styles.button}
+                    onPress={handleRemovePhoto}
+                  >
+                    <Text
+                      style={styles.buttonText}
+                    >
+                      Excluir
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ): <></>
 
+            }
+            <ButtonImage
+              onPress={handleCamera}
+            />
+            <Button
+              title="Enviar"
+              onPress={() => { }}
+              loading={false}
+              disabled={true}
+            />
+
+          </VStack>
         </VStack>
-      </VStack>
+      </ScrollView>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    width: '100%',
+    alignItems: 'center',
+    height: 30,
+    justifyContent: 'center',
+    top: 'auto',
+    bottom: 0
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold'
+  }
+})
--- src/pages/Home/useHome.ts
+++ src/pages/Home/useHome.ts
@@ -1,18 +1,70 @@
-import { Camera, CameraType } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+
 import { useState } from 'react';
 
-export function useHome() {
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [type, setType] = useState(CameraType.back);
+type ImagePropsDTO = {
+  creationTime: number;
+    duration: number;
+    filename: string;
+    height: number;
+    id: string;
+    mediaSubtypes: string[];
+    mediaType: any // Adaptado para aceitar somente 'photo' ou 'video'
+    modificationTime: number;
+    uri: string;
+    width: number;
 
+}
+export function useHome() {
+  const [images, setImages] = useState<ImagePropsDTO[]>([]);
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+  console.log(images)
   async function handleCamera() {
-    if(permission?.granted) {
-     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    if (!permissionResponse?.granted) {
+      requestPermission();  
     } else {
-      requestPermission()
-    }
+      try {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status === 'granted') {
+          const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            quality: 1,
+          });
+
+          if (!result.canceled) {
+            const asset = await MediaLibrary.createAssetAsync(result.assets[0].uri);
+            const newImage: ImagePropsDTO = {
+              creationTime: asset.creationTime,
+              duration: asset.duration ?? 0,
+              filename: asset.filename,
+              height: asset.height,
+              id: asset.id,
+              mediaSubtypes: asset.mediaSubtypes ?? [],
+              mediaType: asset.mediaType,
+              modificationTime: asset.modificationTime,
+              uri: asset.uri,
+              width: asset.width
+          };
+            setImages(prevImages => [...prevImages, newImage]);
+          }
+        } else {
+          console.error('Permissão para acessar a biblioteca de mídia não concedida.');
+        }
+      } catch (error) {
+        console.error('Erro ao acessar a galeria:', error);
+      }
+    } 
   }
+
+  function handleRemovePhoto() {
+    setImages([])
+  }
+
   return {
-    handleCamera
+    images,
+    handleCamera,
+    handleRemovePhoto
   }
 }
